8d00f9162251d313581d4418d0b7375b611da5ad
Daniel Herszenhut <dhersz@gmail.com>
2021-03-02 13:13:29 -0300

    updated package contributors

--- R/gtfsio.R
+++ R/gtfsio.R
@@ -1,5 +1,8 @@
 #' gtfsio: Read and Write General Transit Feed Specification (GTFS) Data
 #'
+#' Provides a \code{gtfs} class to represent General Transit Feed Specification
+#' (GTFS) files, and fast and flexible tools to read and write such files.
+#'
 #' @docType package
 #' @name gtfsio
 #' @aliases gtfsio-package
