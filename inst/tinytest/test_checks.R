ed07cc9b4877aab7030af9080c8eb501a8dd2788
Daniel Herszenhut <dhersz@gmail.com>
2021-03-10 10:47:40 -0300

    more tests

--- inst/tinytest/test_checks.R
+++ inst/tinytest/test_checks.R
@@ -8,8 +8,14 @@ gtfs <- import_gtfs(gtfs_path)
 
 # input checking
 
-expect_error(check_files_exist(unclass(gtfs), "shapes"))
-expect_error(check_files_exist(gtfs, factor("shapes")))
+expect_error(
+  check_files_exist(unclass(gtfs), "shapes"),
+  pattern = "\\'x\\' must inherit from the \\'gtfs\\' class\\."
+)
+expect_error(
+  check_files_exist(gtfs, factor("shapes")),
+  pattern = "\\'files\\' must be a character vector\\."
+)
 
 # expected results
 
@@ -23,8 +29,14 @@ expect_false(check_files_exist(gtfs, c("shapes", "ola")))
 
 # input checking
 
-expect_error(assert_files_exist(unclass(gtfs), "shapes"))
-expect_error(assert_files_exist(gtfs, factor("shapes")))
+expect_error(
+  assert_files_exist(unclass(gtfs), "shapes"),
+  pattern = "\\'x\\' must inherit from the \\'gtfs\\' class\\."
+)
+expect_error(
+  assert_files_exist(gtfs, factor("shapes")),
+  pattern = "\\'files\\' must be a character vector\\."
+)
 
 # expected results
 
@@ -33,6 +45,69 @@ expect_identical(assert_files_exist(gtfs, c("shapes", "trips")), gtfs)
 expect_silent(assert_files_exist(gtfs, "shapes"))
 expect_silent(assert_files_exist(gtfs, c("shapes", "trips")))
 
-expect_error(assert_files_exist(gtfs, "ola"))
-expect_error(assert_files_exist(gtfs, c("shapes", "ola")))
-expect_error(assert_files_exist(gtfs, c("oi", "ola")))
+expect_error(
+  assert_files_exist(gtfs, "ola"),
+  pattern = paste0(
+    "The GTFS object is missing the following ",
+    "required element\\(s\\): \\'ola\\'"
+  )
+)
+expect_error(
+  assert_files_exist(gtfs, c("shapes", "ola")),
+  pattern = paste0(
+    "The GTFS object is missing the following ",
+    "required element\\(s\\): \\'ola\\'"
+  )
+)
+expect_error(
+  assert_files_exist(gtfs, c("oi", "ola")),
+  pattern = paste0(
+    "The GTFS object is missing the following ",
+    "required element\\(s\\): \\'oi\\', \\'ola\\'"
+  )
+)
+
+
+##### CHECK_FIELDS_EXIST
+
+# input checking
+
+expect_error(
+  check_fields_exist(unclass(gtfs), "shapes", "shape_id"),
+  pattern = "\\'x\\' must inherit from the \\'gtfs\\' class\\."
+)
+expect_error(
+  check_fields_exist(gtfs, factor("shapes"), "shape_id"),
+  pattern = paste0(
+    "\\'file\\' must be a string ",
+    "\\(a character vector of length 1\\)\\."
+  )
+)
+expect_error(
+  check_fields_exist(gtfs, c("trips", "shapes"), "shape_id"),
+  pattern = paste0(
+    "\\'file\\' must be a string ",
+    "\\(a character vector of length 1\\)\\."
+  )
+)
+expect_error(
+  check_fields_exist(gtfs, "shapes", factor("shape_id")),
+  pattern = "\\'fields\\' must be a character vector\\."
+)
+
+# 'file' must exist
+
+expect_error(
+  check_fields_exist(gtfs, "oi", "shape_id"),
+  pattern = paste0(
+    "The GTFS object is missing the following ",
+    "required element\\(s\\): \\'oi\\'"
+  )
+)
+
+# expected results
+
+expect_true(check_fields_exist(gtfs, "shapes", "shape_id"))
+expect_true(check_fields_exist(gtfs, "shapes", c("shape_id", "shape_pt_lat")))
+expect_false(check_fields_exist(gtfs, "shapes", "oi"))
+expect_false(check_fields_exist(gtfs, "shapes", c("shape_id", "oi")))
