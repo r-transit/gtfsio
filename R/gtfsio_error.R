4344e8c659c6b076b1f0486362cc30983ea287bb
Daniel Herszenhut <dhersz@gmail.com>
2021-10-25 18:22:13 -0300

    created gtfsio_error()
    this function is used to construct custom error conditions, which aim to
    be more informative than simply using stop().

---
+++ R/gtfsio_error.R
@@ -0,0 +1,25 @@
+#' gtfsio's custom error condition constructor
+#'
+#' @param message The message to inform about the error.
+#' @param subclass The subclass of the error.
+#' @param call A call to associate the error with.
+gtfsio_error <- function(message, subclass, call = sys.call(-1)) {
+
+  # input checking
+
+  if (! (is.character(message) && length(message) == 1))
+    stop("'message' must be a string.")
+
+  if (! is.character(subclass)) stop("'subclass' must be a character vector.")
+
+  # retrieve the function that called 'gtfsio_error' and include it as a class
+
+  fn_name <- as.character(call[[1]])
+  fn_error_class <- paste0(fn_name, "_error")
+
+  subclass <- c(subclass, fn_error_class, "gtfsio_error")
+  error <- errorCondition(message, class = subclass, call = call)
+
+  stop(error)
+
+}
---
+++ inst/tinytest/test_gtfsio_error.R
@@ -0,0 +1,50 @@
+# input checks
+
+expect_error(
+  gtfsio_error(message = c("oi", "ola"), subclass = "test"),
+  class = "simpleError"
+)
+
+expect_error(
+  gtfsio_error(message = "oi", subclass = 1),
+  class = "simpleError"
+)
+
+# error structure
+
+an_error <- tryCatch(
+  gtfsio_error("oi", subclass = "test"),
+  error = function(cnd) cnd
+)
+
+expect_true(
+  all(c("test", "doTryCatch_error", "gtfsio_error") %in% class(an_error))
+)
+expect_equal(an_error$message, "oi")
+
+# checking the function that is assigned to the error
+
+fake_fn <- function(parent = FALSE) {
+  if (!parent) gtfsio_error("oi", "test")
+
+  parent_call <- sys.call(-1)
+  gtfsio_error("oi", "test", call = parent_call)
+}
+
+other_error <- tryCatch(
+  fake_fn(),
+  error = function(cnd) cnd
+)
+expect_inherits(other_error, "fake_fn_error")
+
+# assigning parent function to the error
+
+function_to_assign <- function() fake_fn(parent = TRUE)
+
+last_error <- tryCatch(
+  function_to_assign(),
+  error = function(cnd) cnd
+)
+expect_inherits(last_error, "function_to_assign_error")
+
+
